607ec86f5932dca5ca4c4b7e3b630fe6a51bbe88
Nahuel Gomez Suarez <nahuelgomezs@outlook.com>
2023-08-09 09:58:54 -0300

    Error Logging in PM2

--- src/routes/jwtRoutes.ts
+++ src/routes/jwtRoutes.ts
@@ -14,7 +14,7 @@ router.post("/generate", async (req, res) => {
         res.send(JWT);
     } catch (err) {
         const error = err as Error;
-        console.log("✖ An error was ocurred obtaining JWT");
+        console.error("✖ An error was ocurred obtaining JWT");
         res.status(400).send(error.message);
     }
 });
